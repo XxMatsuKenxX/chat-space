7947f6caf69074d8e4a6a41b5ad442e9223423be
XxmatsukenxX <matsuken521.sn13@icloud.com>
2019-06-26 13:37:18 +0900

    エラーの解決
    グループ作成時、ユーザの値を取得できていなかった。

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
-  belongs_to :group
-  belongs_to :user
+  has_many :group_users
+  has_many :users, through: :group_users
+  validates :name, presence: true, uniqueness: true
 end
